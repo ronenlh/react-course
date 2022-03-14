0956c56c6a43772ec457b926b2a6fbe68cab58ab
Lahat, Ronen (rl240s) <rl240s@intl.att.com>
2022-03-14 20:28:41 +0200

    clean up redux

--- src/index.tsx
+++ src/index.tsx
@@ -1,37 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import Counter from './components/Counter';
-import reducer from './reducers';
-
-const createStore = (reducer: any) => {
-    let state = reducer(undefined, {});
-    let listeners: (() => void)[] = [];
-
-    const getState = () => state;
-
-    const dispatch = (action: any) => {
-        state = reducer(state, action);
-        listeners.forEach((listener) => listener());
-    };
-
-    const subscribe = (listener: () => void) => {
-        listeners.push(listener);
-        return () => {
-            listeners = listeners.filter(l => l !== listener);
-        };
-    };
-
-    return { getState, subscribe, dispatch };
-};
-
-const store = createStore(reducer);
 
 const root = document.getElementById('root');
 const render = () => ReactDOM.render(<Counter
-    value={store.getState()}
-    onIncrement={() => store.dispatch({ type: 'INCREMENT' })}
-    onDecrement={() => store.dispatch({ type: 'DECREMENT' })}
+    value={0}
+    onIncrement={() => console.log('increment')}
+    onDecrement={() => console.log('decrement')}
 />, root);
 
 render();
-store.subscribe(render);
