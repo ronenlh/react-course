b8c5303f1184ea03b544948f2216ec6d6730a318
Lahat, Ronen (rl240s) <rl240s@intl.att.com>
2022-09-12 19:00:48 +0300

    add basic form

--- src/App.tsx
+++ src/App.tsx
@@ -1,29 +1,62 @@
-import React, { useState } from "react";
-import logo from "./logo.svg";
-import "./App.css";
+import React from "react";
+import { Formik } from "formik";
 
-const App = () => {
-  const [name, setName] = useState("");
-
-  return (
-    <form
-      onSubmit={(event) => {
-        alert("A name was submitted: " + name);
-        event.preventDefault();
+const App = () => (
+  <div>
+    <h1>Anywhere in your app!</h1>
+    <Formik
+      initialValues={{ email: "", password: "" }}
+      validate={(values) => {
+        if (!values.email) {
+          return { email: "Required" };
+        } else if (
+          !/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i.test(values.email)
+        ) {
+          return { email: "Invalid email address" };
+        }
+        return {};
+      }}
+      onSubmit={(values, { setSubmitting }) => {
+        setTimeout(() => {
+          alert(JSON.stringify(values, null, 2));
+          setSubmitting(false);
+        }, 400);
       }}
     >
-      <label>
-        Name:
-        <input
-          type="text"
-          name="name"
-          value={name}
-          onChange={(event) => setName(event.target?.value)}
-        />
-      </label>
-      <input type="submit" value="Submit" />
-    </form>
-  );
-};
+      {({
+        values,
+        errors,
+        touched,
+        handleChange,
+        handleBlur,
+        handleSubmit,
+        isSubmitting,
+        /* and other goodies */
+      }) => (
+        <form onSubmit={handleSubmit}>
+          <input
+            type="email"
+            name="email"
+            onChange={handleChange}
+            onBlur={handleBlur}
+            value={values.email}
+          />
+          {errors.email && touched.email && errors.email}
+          <input
+            type="password"
+            name="password"
+            onChange={handleChange}
+            onBlur={handleBlur}
+            value={values.password}
+          />
+          {errors.password && touched.password && errors.password}
+          <button type="submit" disabled={isSubmitting}>
+            Submit
+          </button>
+        </form>
+      )}
+    </Formik>
+  </div>
+);
 
 export default App;
