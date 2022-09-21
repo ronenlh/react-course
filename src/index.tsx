24dbb98f6e635efc88440bf1309aa21c5f5bd7bc
Lahat, Ronen (rl240s) <rl240s@intl.att.com>
2022-09-21 09:18:17 +0300

    update dependencies

--- src/index.tsx
+++ src/index.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import ReactDOM from 'react-dom/client';
 import Counter from './components/Counter';
 import reducer from './reducers';
 
@@ -26,12 +26,14 @@ const createStore = (reducer: any) => {
 
 const store = createStore(reducer);
 
-const root = document.getElementById('root');
-const render = () => ReactDOM.render(<Counter
+const root = ReactDOM.createRoot(document.getElementById('root')!);
+
+const render = () => root.render(<Counter
     value={store.getState()}
     onIncrement={() => store.dispatch({ type: 'INCREMENT' })}
     onDecrement={() => store.dispatch({ type: 'DECREMENT' })}
-/>, root);
+/>);
 
 render();
 store.subscribe(render);
+
